4e548a86ce05c1fc18a844d82113aedafb106869
Opilane TTHK <opilane@tthk.ee>
2024-03-27 14:11:54 +0200

    update erd files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 
+     function triggerDropDown(Id) {
+        var dropdown = document.getElementById(Id);
+        if (dropdown.style.display === "none") {
+            dropdown.style.display = "block";
+        } else {
+            dropdown.style.display = "none";
+        }
+    }
